0b3f202fa1aab04fc3a3ef06049042a463ead78c
Luana Araújo <aninhaluua@gmail.com>
2026-03-11 11:41:23 -0300

    Enhance welcome message formatting in jogar function

--- forca.py
+++ forca.py
@@ -1,6 +1,9 @@
 def jogar():
-    print("***************************")
-    print("bem vindo ao jogo da forca!")
-    print("***************************")
+    print("*********************************")
+    print("***bem vindo ao jogo da forca!***")
+    print("*********************************")
 
     print("fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
